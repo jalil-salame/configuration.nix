31636c058a4b25669b1cde6c6149d90e7ef2050f
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-02-05 20:41:09 +0100

    fix(xdg-portals): Issue opening urls
    See: https://github.com/NixOS/nixpkgs/issues/189851

--- nixos/gui/default.nix
+++ nixos/gui/default.nix
@@ -27,6 +27,10 @@ in
           TimeoutSec = 180;
         };
       };
+      # Fix xdg-portals issue issue: https://github.com/NixOS/nixpkgs/issues/189851
+      systemd.user.extraConfig = ''
+        DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+      '';
 
       fonts.fontDir.enable = true;
 
