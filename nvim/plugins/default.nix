f872b0e0541f5516c9e806d7475038e916a341b9
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-03-02 21:15:55 +0100

    fix(nvim): Tidy up config

--- nvim/plugins/default.nix
+++ nvim/plugins/default.nix
@@ -1,17 +1,24 @@
 {
-  colorizer.enable = true;
-  colorizer.userDefaultOptions.names = false; # disable named colors (i.e. red)
+  colorizer = {
+    enable = true;
+    # disable named colors (i.e. red)
+    userDefaultOptions.names = false;
+  };
   gitsigns.enable = true;
   lspconfig = import ./lspconfig.nix;
   lspkind.enable = true;
   lualine = import ./lualine.nix;
-  luasnip.enable = true;
-  luasnip.extraConfig = { update_events = "TextChanged,TextChangedI"; };
+  luasnip = {
+    enable = true;
+    extraConfig = { update_events = "TextChanged,TextChangedI"; };
+  };
   nvim-cmp = import ./cmp.nix;
   telescope.enable = true;
-  treesitter.enable = true;
-  treesitter.indent = true;
-  treesitter.incrementalSelection.enable = true;
+  treesitter = {
+    enable = true;
+    indent = true;
+    incrementalSelection.enable = true;
+  };
   treesitter-context.enable = true;
   trouble.enable = true;
   lint = {
