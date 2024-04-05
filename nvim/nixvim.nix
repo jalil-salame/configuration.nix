9a4ecf2223e5dc64c95201ba93d9ea1c3a48cd81
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-04-05 11:38:57 +0200

    fix: Renamed option

--- nvim/nixvim.nix
+++ nvim/nixvim.nix
@@ -19,29 +19,33 @@ in
       package = pkgs.neovim-nightly;
       globals.mapleader = " ";
       # Appearance
-      colorschemes.gruvbox.enable = true;
-      colorschemes.gruvbox.settings.bold = true;
-      colorschemes.gruvbox.settings.transparent_mode = true;
-      colorschemes.gruvbox.settings.terminal_colors = true;
-      options.number = true;
-      options.relativenumber = true;
-      options.colorcolumn = "+1";
-      options.cursorline = true;
-      options.wrap = false;
-      options.splitright = true;
-      # Tabs & indentation
-      options.smarttab = true;
-      options.autoindent = true;
-      options.smartindent = true;
-      # Search path
-      options.path = ".,/usr/include,**";
-      options.wildmenu = true;
-      options.hlsearch = true;
-      options.incsearch = true;
-      options.ignorecase = true; # Search ignores cases
-      options.smartcase = true; # Unless it has a capital letter
-      # Enable local configuration :h 'exrc'
-      options.exrc = true; # safe since nvim 0.9
+      colorschemes = {
+        gruvbox.enable = true;
+        gruvbox.settings.bold = true;
+        gruvbox.settings.transparent_mode = true;
+        gruvbox.settings.terminal_colors = true;
+      };
+      opts = {
+        number = true;
+        relativenumber = true;
+        colorcolumn = "+1";
+        cursorline = true;
+        wrap = false;
+        splitright = true;
+        # Tabs & indentation
+        smarttab = true;
+        autoindent = true;
+        smartindent = true;
+        # Search path
+        path = ".,/usr/include,**";
+        wildmenu = true;
+        hlsearch = true;
+        incsearch = true;
+        ignorecase = true; # Search ignores cases
+        smartcase = true; # Unless it has a capital letter
+        # Enable local configuration :h 'exrc'
+        exrc = true; # safe since nvim 0.9
+      };
       plugins = import ./plugins.nix { inherit lib; };
       keymaps = import ./mappings.nix;
       inherit (import ./augroups.nix) autoGroups autoCmd;
