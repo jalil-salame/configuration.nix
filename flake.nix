1f3a666b12572f2dda613f50b00b73651025126f
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-08-18 11:06:23 +0200

    [fix] flake: switch from unstable-small to unstable
    Unstable small breaks too often

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   # Flake inputs
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
-    unstable.url = "nixpkgs/nixos-unstable-small";
+    unstable.url = "nixpkgs/nixos-unstable";
     # Lix
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
