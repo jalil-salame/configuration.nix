6cc9400cadef987ecc436043fbe260dc3327f68f
Jalil David Salamé Messina <jalil.salame@gmail.com>
2025-01-18 23:49:24 +0100

    feat(flake): upgrade lix to v2.92.0
    Yay! :tada:

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # Lix
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.inputs.systems.follows = "systems";
