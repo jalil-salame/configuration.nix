ecc37b8fc306c3fcb24715ff36b025efe8e1d4e1
Jalil David Salamé Messina <jalil.salame@gmail.com>
2025-04-21 21:01:41 +0200

    refactor(example-vm): don't import nixpkgs
    This should save on evaluation time.

--- example-vm/vm-config.nix
+++ example-vm/vm-config.nix
@@ -31,4 +31,6 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
+
+  nixpkgs.hostPlatform = "x86_64-linux";
 }
--- flake-modules/example-vm.nix
+++ flake-modules/example-vm.nix
@@ -1,19 +1,18 @@
 { inputs, lib, ... }:
-let
-  system = "x86_64-linux";
-  overlays = builtins.attrValues inputs.self.overlays;
-  config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "steam-unwrapped" ];
-  pkgs = import inputs.nixpkgs { inherit system overlays config; };
-in
 {
   # Example vm configuration
   flake.nixosConfigurations.vm = lib.nixosSystem {
-    inherit pkgs;
     modules = [
       inputs.self.nixosModules.default
       ../example-vm # import vm configuration
-      { nix.registry.nixpkgs.flake = inputs.nixpkgs; } # pin nixpkgs to the one used by the system
+      {
+        nixpkgs = {
+          overlays = builtins.attrValues inputs.self.overlays;
+          config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "steam-unwrapped" ];
+        };
+        # pin nixpkgs to the one used by the system
+        nix.registry.nixpkgs.flake = inputs.nixpkgs;
+      }
     ];
   };
-
 }
