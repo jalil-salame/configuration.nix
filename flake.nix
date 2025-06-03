557813934abb9b0ca7cb07356ddbc23de37f1056
Jalil David Salamé Messina <jalil.salame@gmail.com>
2025-06-03 18:57:50 +0200

    feat: update lix to 2.93
    I forgot to do this T-T

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     unstable.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
     # Lix
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/release-2.92.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/release-2.93.tar.gz";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.inputs.systems.follows = "systems";
