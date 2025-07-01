54b6d5c5b2233a094e3c2309abc5d1e841209f27
Jalil David Salamé Messina <jalil.salame@gmail.com>
2025-07-01 18:21:22 +0200

    build: add nix-diff and nvd to the devshell
    This will allow me to check what is being included in the derivations
    and why.

--- flake-modules/devshells.nix
+++ flake-modules/devshells.nix
@@ -4,8 +4,11 @@ _: {
     {
       devShells.default = pkgs.mkShellNoCC {
         buildInputs = [
-          pkgs.just
           self'.packages.nvim
+
+          pkgs.just
+          pkgs.nix-diff
+          pkgs.nvd
           pkgs.uv
         ];
         QEMU_OPTS_WL = "-enable-kvm -nodefaults -m 4G -cpu host -smp 4 -device virtio-gpu";
