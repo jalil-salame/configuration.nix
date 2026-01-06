aae25d278fc943a2a3a0d9548693b3eaa4b0419c
Jalil David Salamé Messina <jalil.salame@gmail.com>
2026-01-06 18:29:45 +0100

    refactor!(nvim): switch to blink-cmp
    Use `blink-cmp` instead of `cmp.nvim`, it's supposed to be
    better/faster.
    
    Signed-off-by: Jalil David Salamé Messina <jalil.salame@gmail.com>

--- modules/nixvim/plugins.nix
+++ modules/nixvim/plugins.nix
@@ -1,151 +1,133 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 let
   inherit (lib.nixvim) mkRaw;
 in
 {
-  config.plugins = {
-    cmp =
-      let
-        srcWithIndex = groupIndex: name: { inherit name groupIndex; };
-      in
-      {
+  config = {
+    extraPlugins = [
+      pkgs.vimPlugins.blink-cmp-conventional-commits
+      pkgs.vimPlugins.blink-cmp-spell
+    ];
+
+    plugins = {
+      blink-cmp = {
         enable = true;
-        cmdline = {
-          "/" = {
-            mapping =
-              mkRaw
-                # lua
-                ''
-                  cmp.mapping.preset.cmdline()
-                '';
-            sources = [
-              { name = "rg"; }
-              { name = "buffer"; }
-            ];
-          };
-          ":" = {
-            mapping =
-              mkRaw
-                # lua
-                "cmp.mapping.preset.cmdline()";
-            sources = [
-              { name = "path"; }
-              { name = "cmdline"; }
-            ];
-          };
-        };
         settings = {
-          # Snippets
-          snippet.expand =
-            # lua
-            "function(args) require('luasnip').lsp_expand(args.body) end";
-          # Completion Sources
-          sources = [
-            # very specific (not noisy)
-            (srcWithIndex 1 "calc")
-            (srcWithIndex 1 "crates")
-            (srcWithIndex 1 "fish")
-            (srcWithIndex 1 "luasnip")
-            (srcWithIndex 1 "nvim_lsp")
-            # Generally ok
-            (srcWithIndex 2 "conventionalcommits")
-            (srcWithIndex 2 "nvim_lsp_document_symbol")
-            (srcWithIndex 2 "nvim_lsp_signature_help")
-            # Noisy
-            (srcWithIndex 2 "path")
-            (srcWithIndex 3 "spell")
-            (srcWithIndex 3 "treesitter")
-            # Very noisy
-            (srcWithIndex 4 "buffer")
-          ];
-          mapping =
-            mkRaw
-              # lua
-              ''
-                cmp.mapping.preset.insert({
-                  ["<C-n>"] = function(fallback)
-                    if cmp.visible() then
-                      cmp.select_next_item()
-                    elseif require("luasnip").expand_or_jumpable() then
-                      require("luasnip").expand_or_jump()
-                    elseif has_words_before() then
-                      cmp.complete()
-                    else
-                      fallback()
-                    end
-                  end,
-                  ["<C-p>"] = function(fallback)
-                    if cmp.visible() then
-                      cmp.select_prev_item()
-                    elseif require("luasnip").jumpable(-1) then
-                      require("luasnip").jump(-1)
-                    else
-                      fallback()
-                    end
-                  end,
-                  ["<C-u>"] = cmp.mapping(function(fallback)
-                    if require("luasnip").choice_active() then
-                      require("luasnip").next_choice()
-                    else
-                      fallback()
-                    end
-                  end),
-                  ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                  ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                  ["<C-Space>"] = cmp.mapping.complete { },
-                  ["<C-e>"] = cmp.mapping.close(),
-                  ["<CR>"] = cmp.mapping.confirm { select = true },
-                })
-              '';
+          sources = {
+            default = [
+              "lsp"
+              "snippets"
+              # "calc" # I should make  a plugin
+              "path"
+              "buffer"
+              "spell"
+            ];
+            per_filetype.lua = mkRaw ''{ inherit_defaults = true, "lazydev" }'';
+            providers = {
+              # calc
+              # Commit message suggestions
+              conventional_commits = {
+                name = "Conventional Commits";
+                module = "blink-cmp-conventional-commits";
+                # Only enable on commit messages
+                enabled = mkRaw ''
+                  function()
+                    return vim.bo.filetype == "gitcommit" or vim.bo.filetype == "jjdescription"
+                  end
+                '';
+              };
+              # Neovim specific lua stuff
+              lazydev = {
+                name = "LazyDev";
+                module = "lazydev.integrations.blink";
+                score_offset = 100;
+              };
+              # Spelling suggestions
+              spell = {
+                name = "Spell";
+                module = "blink-cmp-spell";
+              };
+            };
+          };
+          keymap = {
+            preset = "default";
+            "<C-n>" = [
+              "select_next"
+              "snippet_forward"
+              "fallback"
+            ];
+            "<C-p>" = [
+              "select_prev"
+              "snippet_backward"
+              "fallback"
+            ];
+            "<C-d>" = [
+              "scroll_documentation_up"
+              "fallback"
+            ];
+            "<C-f>" = [
+              "scroll_documentation_down"
+              "fallback"
+            ];
+            "<C-Space>" = [
+              "show"
+              "fallback"
+            ];
+            "<C-e>" = [
+              "cancel"
+              "fallback"
+            ];
+            "<CR>" = [
+              "select_and_accept"
+              "fallback"
+            ];
+          };
         };
       };
-    cmp-fish.enable = true;
-    gitsigns.enable = true;
-    lualine = {
-      enable = true;
-      settings.options.theme = lib.mkForce "gruvbox";
-    };
-    luasnip = {
-      enable = true;
-      settings.update_events = "TextChanged,TextChangedI";
-    };
-    noice = {
-      enable = true;
-      settings = {
-        lsp.override = {
-          "vim.lsp.util.convert_input_to_markdown_lines" = true;
-          "vim.lsp.util.stylize_markdown" = true;
-          "cmp.entry.get_documentation" = true;
-        };
-        presets = {
-          # use a classic bottom cmdline for search
-          bottom_search = true;
-          # position the cmdline and popupmenu together
-          command_palette = false;
-          # long messages will be sent to a split
-          long_message_to_split = true;
-          # enables an input dialog for inc-rename.nvim
-          inc_rename = false;
-          # add a border to hover docs and signature help
-          lsp_doc_border = true;
+      gitsigns.enable = true;
+      lazydev.enable = true;
+      lualine = {
+        enable = true;
+        settings.options.theme = lib.mkForce "gruvbox";
+      };
+      noice = {
+        enable = true;
+        settings = {
+          lsp.override = {
+            "vim.lsp.util.convert_input_to_markdown_lines" = true;
+            "vim.lsp.util.stylize_markdown" = true;
+            "cmp.entry.get_documentation" = true;
+          };
+          presets = {
+            # use a classic bottom cmdline for search
+            bottom_search = true;
+            # position the cmdline and popupmenu together
+            command_palette = false;
+            # long messages will be sent to a split
+            long_message_to_split = true;
+            # enables an input dialog for inc-rename.nvim
+            inc_rename = false;
+            # add a border to hover docs and signature help
+            lsp_doc_border = true;
+          };
         };
       };
-    };
-    notify = {
-      enable = true;
-      settings.background_colour = "#000000";
-    };
-    telescope = {
-      enable = true;
-      extensions = {
-        ui-select.enable = true;
-        fzy-native.enable = true;
+      notify = {
+        enable = true;
+        settings.background_colour = "#000000";
       };
+      telescope = {
+        enable = true;
+        extensions = {
+          ui-select.enable = true;
+          fzy-native.enable = true;
+        };
+      };
+      trouble = {
+        enable = true;
+        settings.auto_close = true;
+      };
+      web-devicons.enable = true;
     };
-    trouble = {
-      enable = true;
-      settings.auto_close = true;
-    };
-    web-devicons.enable = true;
   };
 }
--- modules/nixvim/standalone.nix
+++ modules/nixvim/standalone.nix
@@ -61,22 +61,13 @@ in
         plugins.nui-nvim
         plugins.nvim-web-devicons
       ];
-      extraPackages = [ pkgs.luajitPackages.jsregexp ];
       extraConfigLuaPre =
         # lua
         ''
-          -- Lua Pre Config
+          -- START: Lua Pre Config
           if vim.fn.has 'termguicolors' then
             -- Enable RGB colors
             vim.g.termguicolors = true
-          end
-
-          -- Useful function
-          local has_words_before = function()
-            -- unpack = unpack or table.unpack
-            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0
-              and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
           end
           -- END: Lua Pre Config
         '';
